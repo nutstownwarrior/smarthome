03278043690f839e201659748eb73200b6b51f30
nutstownwarrior <schmidfabianlucas@gmx.de>
2020-11-12 12:02:00 +0100

    Update ChinaRelais.ino

--- Arduino Code/test/ChinaRelais/ChinaRelais.ino
+++ Arduino Code/test/ChinaRelais/ChinaRelais.ino
@@ -8,10 +8,10 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266Ping.h>
 
-#define FIREBASE_HOST "smart-home-a5e0f.firebaseio.com"
-#define FIREBASE_AUTH "6bT8EWgOOjlQjmR3Y01hqsHh96juYui8BhyD2d4H"
-#define WIFI_SSID "Shome"
-#define WIFI_PASSWORD "ghjzv23.!?@2019"
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 int n = 0;
 bool button = false;
